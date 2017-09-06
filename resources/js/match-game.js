c88db606063c942a83bd04884c1e281da5fd4360
Ornitorrinco <mel.a.g@icloud.com>
2017-09-06 19:08:10 -0400

    Complete generateCardValues() method.

---
+++ resources/js/match-game.js
@@ -0,0 +1,42 @@
+var MatchGame = {};
+
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+  var orderedValues = [];
+  for(i = 1; i <= 8; i++) {
+    orderedValues.push(i, i);
+  }
+  var cardValues = [];
+  while(orderedValues.length > 0){
+    var randomIndex = Math.floor(Math.random() * orderedValues.length);
+    var randomValue = orderedValues.splice(randomIndex, 1)[0];
+    cardValues.push(randomValue);
+  }
+  return cardValues;
+};
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+
+};
